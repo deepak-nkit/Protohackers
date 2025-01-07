4488374a7559d572616ff720121b7aa28b01f3f0
Deepak Saini <ds954642@gmail.com>
2025-01-07 14:50:02 +0530

    Prime

--- prime_1.py
+++ prime_1.py
@@ -12,7 +12,7 @@ def is_prime(num):
     return True
 
 
-def handle_client(conn , addr):
+def handle_client(conn:socket.socket , addr):
         wrong_data = False
         print(f"connected by {addr}")
         data = ""
@@ -20,9 +20,8 @@ def handle_client(conn , addr):
             msg = conn.recv(1024)
             if not msg:
                 break
+            print("data before decode : " , msg)
             data+= msg.decode('utf-8')
-
-            # req_data  = data.decode("UTF-8").strip()
             print("---------Whole data: ", data)
             while '\n' in data:
                 end = data.index('\n')
@@ -31,15 +30,12 @@ def handle_client(conn , addr):
                 print("---------Remaining data: ", data)
 
                 try:
-                    # requests = req_data.split("\n")
-                    # for request in requests:
                     req = json.loads(req_data.strip())
                     print("~~~~~~~req: ", req)
                     if req.get('method') != 'isPrime' or 'number' not in req:
                         raise ValueError ("Malformed request")
                     num = req.get("number")
                     if not (type(num) == int or type(num) == float):
-                        print("**************************")
                         raise ValueError ("malformed request")
                     response = {
                         "method": "isPrime",
@@ -47,18 +43,14 @@ def handle_client(conn , addr):
                         }
 
                     sending_data = (json.dumps(response) + "\n").encode('utf-8')
-                    print("********* Sending data: ", sending_data)
                     conn.sendall(sending_data)
-                    print("*****completed sending data ")
 
                 except (ValueError):
                     malformed_response = {
                         "method": "isPrime",
                         "prime": "malformed request"
                     }
-                    print("********* Sending  malformed data: " , malformed_response)
                     conn.sendall(("{}lkjsadkfjsajf"+ "\n").encode('utf-8'))
-                    print("****completed sending data ")
                     wrong_data = True
                     break
         conn.shutdown(socket.SHUT_RDWR)                
@@ -83,4 +75,3 @@ def start_server(SERVER_IP = "0.0.0.0",SERVER_PORT = 1672):
 
 if __name__ == "__main__":
     start_server() 
-
