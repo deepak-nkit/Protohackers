cb7067e6b25cd417c7232e7e70938f67c9574ef6
Deepak Saini <ds954642@gmail.com>
2025-01-27 20:05:35 +0530

    "All Bugs Are Fixed { completed }"

--- means_to_an_end.py
+++ means_to_an_end.py
@@ -1,11 +1,6 @@
-import sys
 import struct
-import time
 import socket
 import threading
-
-
-PRICE_DATA = {}
 
 
 def get_bit_val(data: bytearray) -> int:
@@ -26,11 +21,10 @@ def get_bit_val(data: bytearray) -> int:
             val -= 2 ** 32
         if bits[i] == "1":
             val += 2 ** (ln - 1 - i)
-
     return val
 
 
-def mean_Query(minitime: int, maxtime: int) -> int:
+def mean_Query(minitime: int, maxtime: int , PRICE_DATA: dict) -> int:
     sorted_data = {}
     if minitime > maxtime:
         return 0
@@ -43,15 +37,12 @@ def mean_Query(minitime: int, maxtime: int) -> int:
         return 0
     total_sum = sum(sorted_data.values())
     total_count = len(sorted_data)
-    with open("query.txt", "a") as f:
-        f.write(f"Query range: {minitime} to {maxtime} len of data {len(sorted_data)}\n")
-        f.write(str(round(total_sum/total_count)))
-        f.write("\n")
     return round(total_sum / total_count)
 
 
 def handle_client(con: socket.socket, addr):
     data = bytearray()
+    PRICE_DATA = {}
     try:
         while True:
             msg = con.recv(1024)
@@ -64,12 +55,10 @@ def handle_client(con: socket.socket, addr):
                 pair1 = data[1:5]
                 pair2 = data[5:9]
                 data = data[9:]
-                # print(f"Remainig Data:--------   {data}")
 
                 msg_type = chr(first_byte)
 
                 if msg_type not in ["I", "Q"]:
-                    # print("Invalid message Type .... ")
                     continue
 
 
@@ -78,10 +67,6 @@ def handle_client(con: socket.socket, addr):
                     timestamp = get_bit_val(pair1)
                     value = get_bit_val(pair2)
                     # print(f"timestamp: {timestamp} ,  value: {value}")
-                    if value < 0:
-                        print(
-                            "********************************************************************************************************************************************************************"
-                        )
 
                     if timestamp not in PRICE_DATA:
                         PRICE_DATA[timestamp] = value
@@ -96,29 +81,17 @@ def handle_client(con: socket.socket, addr):
                     maxtime = get_bit_val(pair2)
                     print(f"Mintime: {mintime} ,  Maxtime: {maxtime}")
 
-                    if mintime < 0 or maxtime < 0:
-                        print(
-                            "********************************************************************************************************************************************************************"
-                        )
-
-                    query_val = mean_Query(mintime, maxtime)
+                    query_val = mean_Query(mintime, maxtime, PRICE_DATA)
                     if query_val == 0:
                         print("NO value find in give Timestamp! ")
                         con.sendall(struct.pack(">I", 0))
 
                     else:
-                        print("############")
-                        print("value from qurey : ",bytearray(struct.pack(">i", query_val)))
                         con.sendall(bytearray(struct.pack(">i", query_val)))
-                        with open("data.csv", "w") as f:
-                            f.write(str(bytearray(struct.pack(">i", query_val))))
 
                 else:
                     print("Invalid Message type!")
                     break
-
-            # if len(data) > 0 and len(data) < 9:
-            #     print(f"incomplete data from {addr} , waiting for new data.....")
 
     except Exception as e:
         print(f"error handling client {addr}: {e}")
@@ -134,10 +107,10 @@ def start_server(SERVER_IP="0.0.0.0", SERVER_PORT=1672):
         while True:
             try:
                 con, addr = s.accept()
-                # print(f"connecting address {addr}")
+                print(f"connecting address {addr}")
                 threading.Thread(target=handle_client, args=(con, addr)).start()
             except KeyboardInterrupt:
-                # print("Shutdown Server")
+                print("Shutdown Server")
                 break
 
 
