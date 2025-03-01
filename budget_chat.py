59f59dca9596038da9aa6c0f5d17695d9a45f8e7
Deepak Saini <ds954642@gmail.com>
2025-03-01 11:48:54 +0530

    Done Pass Test

--- budget_chat.py
+++ budget_chat.py
@@ -2,7 +2,6 @@ import socket, threading, re
 
 ACTIVE_MEMBERS = {}
 
-
 def set_Member(username: str):
     if not username or not re.match(r"^[a-zA-Z0-9]+$", username):
         return False
@@ -10,20 +9,18 @@ def set_Member(username: str):
         return False
     return True
 
-
 def broadCast_message(message: str, username: str):
-    for user, sock in ACTIVE_MEMBERS.items():
+    for user, sock in list(ACTIVE_MEMBERS.items()):
         if user != username:
-            sock.sendall(message.encode("ASCII"))
-
-
+            sock.sendall(message.encode("utf-8"))
+              
 def handle_client(conn: socket.socket, addr):
+
     #  first Receive the username:
     conn.sendall(b"Welcome to budgetchat! What shall I call you?\n")
     username = None
-
     try:
-        msg = conn.recv(1024).decode("ASCII").strip()
+        msg = conn.recv(1024).decode("UTF-8").strip()
 
         if not set_Member(msg):
             conn.sendall(b"Invalid Usernmae! Use only Characters and numbers")
@@ -35,53 +32,48 @@ def handle_client(conn: socket.socket, addr):
             return
 
         username = msg
-        ACTIVE_MEMBERS[username] = conn
-        # print(ACTIVE_MEMBERS)
-        print(f" {username} has joined from {addr}")
 
-        # Announce member names:
+        # Announce Joined Username :
+        join_msg = f"* {username} has entered the room\n"
+        print(f"*** { join_msg}")
+        broadCast_message(join_msg, username)
+        ACTIVE_MEMBERS[username] = conn
+
+        # Announce presented Member names:
         presented_user = ", ".join(user for user in ACTIVE_MEMBERS.keys() if user != username)
         members_names = f"* The room contains: {presented_user}\n"
-        print(f"~~~\t~~: The User {username} -----  the announcement member {presented_user}\n ")
         conn.sendall(members_names.encode("utf-8"))
 
-        # Presence notification:
-        join_msg = f"* {username} has entered the room"
-        broadCast_message(join_msg, username)
-
-
         # Handle message:
+
         data = ""
         while True:
-
-            msg = conn.recv(1024)
-            if not msg:
-                break
-            data += msg.decode("UTF-8").strip()
-            print(f"Data: {data}")
-            while "\n" in data:
-                end = data.index("\n")
-                req_data = data[:end]
-                data = data[end + 1 :]
-                sender_message = f"[{username}] {req_data}"
-                print(sender_message)
-                broadCast_message(sender_message, username)
+                msg = conn.recv(1024)
+                if not msg:
+                    break
+                data += msg.decode("utf-8")            
+                while '\n' in data:
+                    # print("*** : ",data)
+                    end = data.index("\n")
+                    req_data = data[:end]
+                    data = data[end + 1 :]
+                    if req_data:
+                        broadCast_message(f"[{username}] {req_data + "\n"}", username)
 
     except Exception as e:
         print(f"{username} has disconnected.")
 
-        # Remove User and Notify others:
+    # Remove User and Notify others:
     finally:
         print("In Final Block")
         if username in ACTIVE_MEMBERS:
             del ACTIVE_MEMBERS[username]
-            broadCast_message(f"* {username} has left the chat", username)
+            broadCast_message(f"* {username} has left the chat\n", username)
 
         conn.close()
         print(f"Connection Closed for {username}")
 
-
-def start_server(SERVER_IP="0.0.0.0", SERVER_PORT=1672):
+def start_server(SERVER_IP ="0.0.0.0", SERVER_PORT =1672):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((SERVER_IP, SERVER_PORT))
         s.listen(10)
@@ -95,7 +87,6 @@ def start_server(SERVER_IP="0.0.0.0", SERVER_PORT=1672):
 
         except KeyboardInterrupt:
             print("ShutDown the Server ")
-
 
 if __name__ == "__main__":
     start_server()
